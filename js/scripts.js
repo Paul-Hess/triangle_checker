de26c7312b7741efabdc07de24fa2a0d94de187c
David Remington and Paul Hess <david.ian.remington@gmail.com>
2016-01-19 16:06:07 -0800

    added html stucture

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
+ var triangleReturn = " "
+
 var triangle = function(side1, side2, side3) {
   if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
-    return "not a triangle";
+    triangleReturn = "not a triangle";
   } else if (side1 === side2 && side1 === side3) {
-    return "equilateral";
+    triangleReturn = "equilateral";
   } else if (side1 === side2 && side1 !== side3) {
-    return "isosceles";
+    triangleReturn = "isosceles";
   } else if (side1 !== side2 && side1 !== side3) {
-    return "scalene";
+    triangleReturn = "scalene";
   };
+  return triangleReturn;
 };
+
+$(function() {
+  $("form#triangle").submit(function(event) {
+    var sideInput1 = $('input#side1').val();
+    var sideInput2 = $('input#side2').val();
+    var sideInput3 = $('input#side3').val();
+    parseFloat(sideInput1);
+    parseFloat(sideInput2);
+    parseFloat(sideInput3);
+    triangle(sideInput1, sideInput2, sideInput3);
+
+    $('span.results').empty().text(triangleReturn);
+
+    event.preventDefault();
+  });
+});
